557dfdb3e1ee5df4d91260ed136d3416c6f4e005
Sebastian Skałacki <skalee@gmail.com>
2018-10-28 18:55:53 +0100

    Define an ActiveRecord data type
    Since Rails 4.2, Rails allows for defining custom data types.  This
    feature replaces old hacks, which have been finally removed in Rails
    5.2.  For more information, see RDoc introduced in this commit, and
    this comment:
    https://github.com/rails/rails/pull/27962#issuecomment-392578427

--- lib/activeuuid.rb
+++ lib/activeuuid.rb
@@ -1,5 +1,6 @@
 require "activeuuid/version"
 require "activeuuid/utils"
+require "activeuuid/attribute_type"
 require "activeuuid/patches"
 require "activeuuid/uuid"
 require "activeuuid/railtie" if defined?(Rails::Railtie)
---
+++ spec/unit/attribute_type_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+# Type assertions are necessary because you can't tell UUID and String apart
+# with regular ==.
+describe ActiveUUID::AttributeType do
+  let(:uuid) { UUIDTools::UUID.parse(hex_with_dashes) }
+  let(:hex_with_dashes) { "472b22d4-9fa3-45c4-86cd-2f2cdf77d485" }
+  let(:hex_without_dashes) { hex_with_dashes.delete("-").upcase }
+  let(:binary_string) { uuid.raw }
+
+  let(:instance) { described_class.new }
+
+  describe "#cast" do
+    subject { instance.method(:cast) }
+    example { expect(subject.(uuid)).to be_respective_uuid }
+    example { expect(subject.(hex_with_dashes)).to be_respective_uuid }
+    example { expect(subject.(hex_without_dashes)).to be_respective_uuid }
+    example { expect(subject.(nil)).to be(nil) }
+  end
+
+  describe "#serialize" do
+    subject { instance.method(:serialize) }
+    example { expect(subject.(uuid)).to be_respective_binary }
+    example { expect(subject.(nil)).to be(nil) }
+  end
+
+  describe "#deserialize" do
+    subject { instance.method(:deserialize) }
+    example { expect(subject.(binary_string)).to be_respective_uuid }
+    example { expect(subject.(nil)).to be(nil) }
+  end
+
+  def be_respective_uuid
+    eq(uuid) & be_instance_of(UUIDTools::UUID)
+  end
+
+  def be_respective_binary
+    eq(binary_string) & be_instance_of(::ActiveRecord::Type::Binary::Data)
+  end
+end
