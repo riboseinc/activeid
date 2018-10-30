5613fa4d5e1198b1a05f54e7607cba4f673e90ac
Sebastian Skałacki <skalee@gmail.com>
2018-10-30 17:15:25 +0100

    Measure test coverage with SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+require "simplecov"
+SimpleCov.start
+
+if ENV.key?("CI")
+  require "codecov"
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require "rubygems"
 require "bundler/setup"
 
