c4639bd2bee75aa86bb84d69569ee24342696135
Sebastian Skałacki <skalee@gmail.com>
2018-12-14 01:58:06 +0100

    Split large test file

--- spec/integration/model_with_uuids_spec.rb
+++ spec/integration/model_with_uuids_spec.rb
@@ -1,48 +1,5 @@
 require "spec_helper"
 
-describe Article do
-  let!(:article) { Fabricate :article }
-  let(:id) { article.id }
-  let(:model) { Article }
-  subject { model }
-
-  context "model" do
-    its(:all) { should == [article] }
-    its(:first) { should == article }
-  end
-
-  context "existance" do
-    subject { article }
-    its(:id) { should be_a Integer }
-  end
-
-  context ".find" do
-    specify { expect(model.find(id)).to eq(article) }
-  end
-
-  context ".where" do
-    specify { expect(model.where(id: id).first).to eq(article) }
-  end
-
-  context "#destroy" do
-    subject { article }
-    its(:delete) { should be_truthy }
-    its(:destroy) { should be_truthy }
-  end
-
-  context "#save" do
-    subject { article }
-    let(:array) { [1, 2, 3] }
-
-    its(:save) { should be_truthy }
-
-    context "when change array field" do
-      before { article.some_array = array }
-      its(:save) { should be_truthy }
-    end
-  end
-end
-
 describe UuidArticle do
   let!(:article) { Fabricate :uuid_article }
   let!(:id) { article.id }
@@ -136,26 +93,5 @@ describe UuidArticle do
         expect(article.another_uuid).to eq(uuid)
       end
     end
-  end
-end
-
-describe UuidArticleWithNaturalKey do
-  let!(:article) { Fabricate :uuid_article_with_natural_key }
-  let!(:id) { article.id }
-  let!(:uuid) { UUIDTools::UUID.sha1_create(UUIDTools::UUID_OID_NAMESPACE, article.title) }
-  subject { article }
-  context "natural_key" do
-    its(:id) { should == uuid }
-  end
-end
-
-describe UuidArticleWithNamespace do
-  let!(:article) { Fabricate :uuid_article_with_namespace }
-  let!(:id) { article.id }
-  let!(:namespace) { UuidArticleWithNamespace._uuid_namespace }
-  let!(:uuid) { UUIDTools::UUID.sha1_create(namespace, article.title) }
-  subject { article }
-  context "natural_key_with_namespace" do
-    its(:id) { should == uuid }
   end
 end
