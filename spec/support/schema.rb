3afe1c6053b98a810b7035c7fefee462983f23e2
Sebastian Skałacki <skalee@gmail.com>
2018-12-19 06:54:02 +0100

    Force creation of test database tables
    Running tests in development is much easier when one doesn't need to
    drop tables manually before running RSpec.

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -1,5 +1,5 @@
 ActiveRecord::Schema.define do
-  create_table :articles do |t|
+  create_table :articles, force: true do |t|
     t.string :title
     t.text :body
     t.column :some_array, :integer, array: true
@@ -7,7 +7,7 @@ ActiveRecord::Schema.define do
     t.timestamps
   end
 
-  create_table :uuid_articles, id: false do |t|
+  create_table :uuid_articles, id: false, force: true do |t|
     t.uuid :id, primary_key: true
     t.string :title
     t.text :body
