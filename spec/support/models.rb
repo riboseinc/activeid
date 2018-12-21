ae40c96c4ac920bbf2cec722f205d8ae06935857
Sebastian Skałacki <skalee@gmail.com>
2018-12-21 04:39:08 +0100

    Remove code duplications in test models

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -5,15 +5,10 @@ class UuidArticle < ActiveRecord::Base
   include ActiveUUID::UUID
 end
 
-class UuidArticleWithNaturalKey < ActiveRecord::Base
-  include ActiveUUID::UUID
-  self.table_name = "uuid_articles"
+class UuidArticleWithNaturalKey < UuidArticle
   natural_key :title
 end
 
-class UuidArticleWithNamespace < ActiveRecord::Base
-  include ActiveUUID::UUID
-  self.table_name = "uuid_articles"
-  natural_key :title
+class UuidArticleWithNamespace < UuidArticleWithNaturalKey
   uuid_namespace "45e676ea-8a43-4ffe-98ca-c142b0062a83" # a random UUID
 end
