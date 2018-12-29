48b5c095e2b826b64e58892a02fc0976cafecf85
Sebastian Skałacki <skalee@gmail.com>
2018-12-29 18:06:59 +0100

    Extract RSpec examples into shared blocks
    Extract some integration specs into shared blocks for easy re-using.

--- spec/integration/examples_for_uuid_models_having_namespaces.rb
+++ spec/integration/examples_for_uuid_models_having_namespaces.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
-describe UuidArticleWithNamespace do
-  let!(:article) { Fabricate :uuid_article_with_namespace }
+shared_examples "model with UUIDs and a namespace" do
+  let!(:article) { Fabricate model.name.underscore }
   let!(:id) { article.id }
-  let!(:namespace) { UuidArticleWithNamespace._uuid_namespace }
+  let!(:namespace) { model._uuid_namespace }
   let!(:uuid) { UUIDTools::UUID.sha1_create(namespace, article.title) }
   subject { article }
   context "natural_key_with_namespace" do
