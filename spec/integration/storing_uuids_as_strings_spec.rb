8336ed0980bc8114d811ea2518f514a82a149ef8
Sebastian Skałacki <skalee@gmail.com>
2018-12-29 19:06:14 +0100

    Support storing UUIDs as strings
    Apart from supporting regular varchar columns, this also works with
    PostgreSQL's native UUID data type.

---
+++ spec/integration/storing_uuids_as_strings_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+Dir["#{__dir__}/examples_*.rb"].each { |f| require f }
+
+describe "storing simple UUIDs as strings" do
+  include_examples "model with UUIDs" do
+    let(:model) { StringUuidArticle }
+    let(:quoted_article_id) { article.id.to_s }
+  end
+end
+
+describe "storing UUIDs with a natural key as strings" do
+  include_examples "model with UUIDs and a natural key" do
+    let(:model) { StringUuidArticleWithNaturalKey }
+  end
+end
+
+describe "storing UUIDs with a namespace as strings" do
+  include_examples "model with UUIDs and a namespace" do
+    let(:model) { StringUuidArticleWithNamespace }
+  end
+end
