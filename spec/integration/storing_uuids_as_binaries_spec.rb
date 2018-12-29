64877a23ce6c3b014858590b376cc494dccd8e1a
Sebastian Skałacki <skalee@gmail.com>
2018-12-29 18:51:53 +0100

    Define more models for testing
    Define models in order to test various ways of UUID storage.

--- spec/integration/storing_uuids_as_binaries_spec.rb
+++ spec/integration/storing_uuids_as_binaries_spec.rb
@@ -4,19 +4,19 @@ Dir["#{__dir__}/examples_*.rb"].each { |f| require f }
 
 describe "storing simple UUIDs as binaries" do
   include_examples "model with UUIDs" do
-    let(:model) { UuidArticle }
+    let(:model) { BinaryUuidArticle }
     let(:quoted_article_id) { ActiveUUID.quote_as_binary(article.id) }
   end
 end
 
 describe "storing UUIDs with a natural key as binaries" do
   include_examples "model with UUIDs and a natural key" do
-    let(:model) { UuidArticleWithNaturalKey }
+    let(:model) { BinaryUuidArticleWithNaturalKey }
   end
 end
 
 describe "storing UUIDs with a namespace as binaries" do
   include_examples "model with UUIDs and a namespace" do
-    let(:model) { UuidArticleWithNamespace }
+    let(:model) { BinaryUuidArticleWithNamespace }
   end
 end
