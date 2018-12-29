4e6a800c3822135ddeb422d66e30bb9726153160
Sebastian Skałacki <skalee@gmail.com>
2018-12-29 19:14:33 +0100

    Disable specs for binary storage for PostgreSQL
    Binary storage won't work with PostgreSQL.  The reason is that
    ActiveRecord::Type::Binary won't work with PostgreSQL either.
    
    This isn't a big deal though.  In PostgreSQL, one should use native UUID
    SQL type, and compatible string storage in ActiveUUID.

--- spec/integration/storing_uuids_as_binaries_spec.rb
+++ spec/integration/storing_uuids_as_binaries_spec.rb
@@ -3,6 +3,10 @@ require "spec_helper"
 Dir["#{__dir__}/examples_*.rb"].each { |f| require f }
 
 describe "storing simple UUIDs as binaries" do
+  if ENV["DB"] == "postgresql"
+    before { skip "Binary UUID storage does not work in PostgreSQL" }
+  end
+
   include_examples "model with UUIDs" do
     let(:model) { BinaryUuidArticle }
     let(:quoted_article_id) { ActiveUUID.quote_as_binary(article.id) }
@@ -10,12 +14,20 @@ describe "storing simple UUIDs as binaries" do
 end
 
 describe "storing UUIDs with a natural key as binaries" do
+  if ENV["DB"] == "postgresql"
+    before { skip "Binary UUID storage does not work in PostgreSQL" }
+  end
+
   include_examples "model with UUIDs and a natural key" do
     let(:model) { BinaryUuidArticleWithNaturalKey }
   end
 end
 
 describe "storing UUIDs with a namespace as binaries" do
+  if ENV["DB"] == "postgresql"
+    before { skip "Binary UUID storage does not work in PostgreSQL" }
+  end
+
   include_examples "model with UUIDs and a namespace" do
     let(:model) { BinaryUuidArticleWithNamespace }
   end
