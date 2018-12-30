dbed5eec5ac2afe544422eb1c8068f52a8136b8c
Sebastian Skałacki <skalee@gmail.com>
2018-12-30 19:23:04 +0100

    Improve relative paths expansion in spec helper
    Use File.expand_path and __dir__ instead of __FILE__.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,11 +13,11 @@ Bundler.require :development
 
 require "activeuuid"
 
-ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
-ActiveRecord::Base.configurations = YAML::safe_load(File.read(File.dirname(__FILE__) + "/support/database.yml"))
+ActiveRecord::Base.logger = Logger.new(File.expand_path("debug.log", __dir__))
+ActiveRecord::Base.configurations = YAML::safe_load(File.read(File.expand_path("support/database.yml", __dir__)))
 ActiveRecord::Base.establish_connection(ENV["DB"].to_sym)
 
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir[File.expand_path("support/**/*.rb", __dir__)].each { |f| require f }
 
 RSpec.configure do |config|
   # Remove this line if you don't want RSpec's should and should_not
