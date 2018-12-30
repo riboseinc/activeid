billr578/activeuuid

Define an ActiveRecord data type
Since Rails 4.2, Rails allows for defining custom data types.  This
feature replaces old hacks, which have been finally removed in Rails
5.2.  For more information, see RDoc introduced in this commit, and
this comment:
https://github.com/rails/rails/pull/27962#issuecomment-392578427

lib/activeuuid.rb
@@ -1,6 +1,7 @@
 require "activeuuid/version"
 require "activeuuid/patches"
 require "activeuuid/uuid"
+require "activeuuid/column_type"
 require "activeuuid/railtie" if defined?(Rails::Railtie)
 require "pp"
 

riboseinc/activeid

lib/activeuuid.rb
@@ -1,5 +1,6 @@
 require "activeuuid/version"
 require "activeuuid/utils"
+require "activeuuid/attribute_type"
 require "activeuuid/patches"
 require "activeuuid/uuid"
 require "activeuuid/railtie" if defined?(Rails::Railtie)