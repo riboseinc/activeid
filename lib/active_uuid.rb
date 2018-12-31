67ceb77de4f984aa53a9801f5aae2be02594de71
Sebastian Skałacki <skalee@gmail.com>
2018-12-31 11:33:05 +0100

    Allow loading gem with and without patches
    Do not load any monkey patches when requiring "active_uuid", but load
    all of them when requiring "active_uuid/all".

--- lib/active_uuid.rb
+++ lib/active_uuid.rb
@@ -1,7 +1,6 @@
 require "active_uuid/version"
 require "active_uuid/utils"
 require "active_uuid/attribute_type"
-require "active_uuid/connection_patches"
 require "active_uuid/model"
 require "active_uuid/railtie" if defined?(Rails::Railtie)
 require "pp"
