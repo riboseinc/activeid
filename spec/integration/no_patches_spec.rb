320060861da9bc7a0a3170b7d4d8eda3ecee53f0
Sebastian Skałacki <skalee@gmail.com>
2019-01-02 17:42:03 +0100

    Test if monkey patches are loaded or not
    Make sure that if ActiveUUID is loaded without patches, then
    ActiveRecord is not altered indeed.

---
+++ spec/integration/no_patches_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+RSpec.describe "without monkey patches" do
+  before do
+    unless ENV.fetch("NO_PATCHES", false)
+      skip "Monkey patching is enabled in this build"
+    end
+  end
+
+  it "does not patch ActiveRecord connection" do
+    db_conn = ActiveRecord::Base.connection
+    db_conn_modules = db_conn.singleton_class.ancestors
+
+    expect(db_conn_modules).to all(satisfy { |m| /ActiveUUID/ !~ m.name })
+  end
+
+  it "does not patch Table class" do
+    table_modules = ActiveRecord::ConnectionAdapters::Table.ancestors
+    expect(table_modules).to all(satisfy { |m| /ActiveUUID/ !~ m.name })
+  end
+
+  it "does not patch TableDefinition class" do
+    table_modules = ActiveRecord::ConnectionAdapters::TableDefinition.ancestors
+    expect(table_modules).to all(satisfy { |m| /ActiveUUID/ !~ m.name })
+  end
+end
