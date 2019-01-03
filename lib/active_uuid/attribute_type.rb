97a6ae6bff576121ca7a750797e618683e2f50d9
Sebastian Skałacki <skalee@gmail.com>
2019-01-03 17:43:43 +0100

    Change file indentation
    This commit affects only white spaces.  This way commits with
    refactoring will not be cluttered, and Git will track file renames
    correctly.

--- lib/active_uuid/attribute_type.rb
+++ lib/active_uuid/attribute_type.rb
@@ -6,58 +6,58 @@ module ActiveUUID
   # See docs for +::ActiveRecord::Attributes::ClassMethods#attribute+ for usage.
   # See docs for +::ActiveRecord::Type::Value+ for meaning of overriden methods.
   class AttributeType < ::ActiveRecord::Type::Value
-    attr_reader :storage_type
+      attr_reader :storage_type
 
-    delegate :cast_to_uuid, to: ActiveUUID::Utils
-    delegate :serialize, :deserialize, to: :storage_type, prefix: :s
+      delegate :cast_to_uuid, to: ActiveUUID::Utils
+      delegate :serialize, :deserialize, to: :storage_type, prefix: :s
 
-    def initialize(storage_mode)
-      @storage_type = instantiate_storage(storage_mode)
-    end
-
-    # Converts UUID or its string representation into a binary value (see
-    # super).
-    def serialize(value)
-      s_serialize(cast_to_uuid(value))
-    end
-
-    # Converts binary values into UUIDs.
-    def deserialize(value)
-      cast_to_uuid(s_deserialize(value))
-    end
-
-    # Converts strings into UUIDs on user input assignment, called internally
-    # from #cast.
-    def cast_value(value)
-      cast_to_uuid(value)
-    end
-
-    private
-
-    # Returns a new instance of attribute type appropriate for requested storage
-    # mode.
-    def instantiate_storage(storage_mode)
-      case storage_mode
-      when :binary then BinaryStorage.new
-      when :string then StringStorage.new
-      else
-        raise ArgumentError, "Incorrect storage mode.  It must be " +
-          "either :binary or :string, but #{storage_mode.inspect} was passed."
+      def initialize(storage_mode)
+        @storage_type = instantiate_storage(storage_mode)
       end
-    end
 
-    # Wrapper for `ActiveRecord::Type::Binary` which (de)serializes UUIDs.
-    class BinaryStorage < ::ActiveRecord::Type::Binary
-      def serialize(uuid_or_nil)
-        super(uuid_or_nil&.raw)
+      # Converts UUID or its string representation into a binary value (see
+      # super).
+      def serialize(value)
+        s_serialize(cast_to_uuid(value))
       end
-    end
 
-    # Wrapper for `ActiveModel::Type::String` which (de)serializes UUIDs.
-    class StringStorage < ActiveRecord::Type::String
-      def serialize(uuid_or_nil)
-        super(uuid_or_nil&.to_s)
+      # Converts binary values into UUIDs.
+      def deserialize(value)
+        cast_to_uuid(s_deserialize(value))
       end
-    end
+
+      # Converts strings into UUIDs on user input assignment, called internally
+      # from #cast.
+      def cast_value(value)
+        cast_to_uuid(value)
+      end
+
+      private
+
+      # Returns a new instance of attribute type appropriate for requested storage
+      # mode.
+      def instantiate_storage(storage_mode)
+        case storage_mode
+        when :binary then BinaryStorage.new
+        when :string then StringStorage.new
+        else
+          raise ArgumentError, "Incorrect storage mode.  It must be " +
+            "either :binary or :string, but #{storage_mode.inspect} was passed."
+        end
+      end
+
+      # Wrapper for `ActiveRecord::Type::Binary` which (de)serializes UUIDs.
+      class BinaryStorage < ::ActiveRecord::Type::Binary
+        def serialize(uuid_or_nil)
+          super(uuid_or_nil&.raw)
+        end
+      end
+
+      # Wrapper for `ActiveModel::Type::String` which (de)serializes UUIDs.
+      class StringStorage < ActiveRecord::Type::String
+        def serialize(uuid_or_nil)
+          super(uuid_or_nil&.to_s)
+        end
+      end
   end
 end
